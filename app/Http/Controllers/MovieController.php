00a2a71aa00ed44a8f4d6a7bd44240cd97585171
DesfanniZulya <desfannizulya@gmail.com>
2025-05-14 16:35:35 +0700

    detail movie

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -8,7 +8,9 @@ use Illuminate\Http\Request;
 class MovieController extends Controller
 {
   public function index(){
-    $movies = Movie::latest()->take(9)->get(); // Ambil 9 film terbaru
-    return view('layouts.template', compact('movies'));
+    // $movies = Movie::latest() memanggil semua
+     $movies = Movie::latest()->paginate(6); // Ambil 6 film terbaru
+    return view('homepage', compact('movies'));
+   // return view('homepage, ['movie'-> $movies]);
   }
 }
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,14 +1,34 @@
-<div class="card mb-3" style="max-width: 540px;">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img src="..." class="img-fluid rounded-start" alt="...">
-      </div>
-      <div class="col-md-8">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-        </div>
+@extends('layouts.template')
+
+@section('title', 'Homepage')
+
+@section('content')
+
+    <h1>Latest Movie</h1>
+    <div class="row">
+        @foreach($movies as $movie)
+        <div class="col-lg-6">
+
+        <div class="card mb-3" style="max-width: 540px;">
+     <div class="row g-0">
+    <div class="col-md-4">
+      <img src="{{$movie->cover_image}}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">{{$movie->title}}</h5>
+        <p class="card-text">{{Str::words($movie->synopsis, 15)}}</p>
+        <p class="card-text"><small class="text-body-secondary">Year: {{$movie->year}}</small></p>
+        <a href="/movie/{{ $movie->id }}/{{ $movie->slug }}"class="btn btn-success">Read More </a>
       </div>
     </div>
+    </div>
   </div>
+</div>
+@endforeach
+<div>
+    {{ $movies->links() }}
+</div>
+    </div>
+
+@endsection
