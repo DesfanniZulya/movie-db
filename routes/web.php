2b49cb2dfb07d84bf5125ce97e72ca73d5a764f9
DesfanniZulya <desfannizulya@gmail.com>
2025-05-28 14:01:21 +0700

    logout

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,5 +12,14 @@ Route::get('/', function () {
 Route::get('/', [MovieController::class, 'index']);
 
 Route::get('/movie/{id}/{slug}', [MovieController::class, 'detail_movie']);
-Route::get('/movie/create', [MovieController::class, 'create']);
-Route::get('/movie/create',[MovieController::class,'create']);
+
+Route::get('/movie/create', [MovieController::class, 'create'])->middleware('auth');
+
+Route::Post('/movie/store',[MovieController::class,'store'])->middleware('auth');
+
+Route::get('/login', [AuthController::class,'formLogin'])->name('login');
+
+Route::post('/login', [AuthController::class,'login']);
+Route::post('/logout', [AuthController::class, 'destroy'])->name('logout');
+
+
